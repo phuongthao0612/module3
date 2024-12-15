6870f78b0f792f2639cd3baacb9a86dd926ebae9
phuongthao0612 <phuong6thao@gmail.com>
2024-12-15 22:15:04 +0700

    upbai

---
+++ ss2/bai1.sql
@@ -0,0 +1,56 @@
+create database quanly_donhang;
+use quanly_donhang;
+create table phieuxuat(
+soPX int primary key,
+ngayxuat date
+);
+create table vattu(
+maVTU int primary key,
+tenVTU varchar (255)
+);
+create table donDH(
+soDH int primary key,
+ngayDH date
+);
+create table phieunhap(
+soPN int primary key,
+ngaynhap date
+);
+create table nhaCC(
+maNCC int primary key,
+tenNCC varchar (255),
+diachi varchar (255),
+sdt varchar (15)
+);
+create table chitiet_phieuxuat(
+soPX int,
+maVTU int,
+DGXuat decimal (10, 2), 
+SLXuat int,
+primary key (soPX, maVTU),
+foreign key (soPX) references phieuxuat (soPX),
+foreign key (maVTU) references vattu (maVTU)
+);
+create table chitiet_phieunhap(
+maVTU int, 
+soPN int, 
+DGNhap decimal (10, 2),
+SLNhap int,
+primary key (maVTU, soPN),
+foreign key (soPN) references phieunhap (soPN),
+foreign key (maVTU) references vattu (maVTU)
+);
+create table chitiet_dondh(
+maVTU int, 
+soDH int,
+primary key (maVTU, soDH),
+foreign key (soDH) references donDH(soDH),
+foreign key (maVTU) references vattu(maVTU)
+);
+create table cungcap(
+maNCC INT,
+maVTU int,
+primary key (maNCC, maVTU),
+foreign key (maNCC) references nhaCC(maNCC),
+foreign key (maVTU) references vattu(maVTU)
+);
---
+++ ss2/bai2.sql
@@ -0,0 +1,27 @@
+create database quanly_banhang;
+use quanly_banhang;
+create table custumers(
+cID int primary key,
+cName varchar(255),
+cAge int
+);
+create table orders(
+oID int primary key,
+cID int,
+oDate date,
+oTotalPrice decimal(10, 2),
+foreign key (cID) references custumers(cID)
+);
+create table products(
+pID int primary key,
+pName varchar(255),
+pPrice decimal(10, 2)
+);
+create table orderdetails(
+oID int,
+pID int,
+odQTY int,
+primary key (oID, pID),
+foreign key (oID) references orders(oID),
+foreign key (pID) references products(pID)
+);
